805811066a8b15c3d850bd7ea8615aa784376bc4
David Parrish <daveparrish@tutanota.com>
2018-02-01 13:14:12 -0500

    Clean up removeUserTest

--- src-test/CLITest.hs
+++ src-test/CLITest.hs
@@ -32,23 +32,21 @@ removeUserTest :: Assertion
 removeUserTest = withTemporaryTestDirectory tempDir ( \_ -> do
     -- Simulate a create user and then removing a user.
     -- For example:
-    --   $ dmss-cli id create -n "donald_trump"
+    --   $ dmss-cli id create -n "donald_trump" -p "password"
     --   $ dmss-cli id list
-    --   NAME           EMAIL                         FINGERPRINT
-    --   donald_trum...                               D8F162CB21A2A66BC75A4E6DC07272592E46EA59
-    --   $ dmss-cli id remove D8F162CB21A2A66BC75A4E6DC07272592E46EA59
+    --   NAME
+    --   donald_trum...
+    --   $ dmss-cli id remove donald_trump
     _ <- processIdCreate "donald_trump" "topSecret!"
 
     -- Get the fingerprint from result of `processIdList`
     l <- processIdList
     length (lines l) @?= 2
-    print l
 
     -- Remove the created user ID
     _ <- processIdRemove "donald_trump"
 
     -- Simply check that no results are returned
     l' <- processIdList
-    print l'
     length (lines l') @?= 1
   )
