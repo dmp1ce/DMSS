00e6c4cc0bd163662d1400ed7eb12eb9099ce86c
David Parrish <daveparrish@tutanota.com>
2018-03-29 13:54:10 -0400

    Add dev tool for running and watching tests

--- README.md
+++ README.md
@@ -7,3 +7,23 @@ happiness. Neither stability nor quality of either API or implementation is
 guaranteed at the moment.**
 
 See [DESIGN.md](DESIGN.md) for now.
+
+## Development
+
+For active development use the `dev` application to watch for file changes and running test continually.
+
+Build `dev`.
+
+`stack build :dev`
+
+To watch a subset of tests.
+
+`stack exec -- dev test -w -p "my test filter"`
+
+Or run all the tests.
+
+`stack exec -- dev test`
+
+See `--help` for details.
+
+`stack exec -- dev --help`
---
+++ src-bin/dev.hs
@@ -0,0 +1,60 @@
+{-
+Run DMSS tests specified
+-}
+
+import Turtle
+import Data.List (intersperse)
+import Prelude hiding (concat)
+import Data.Text (concat)
+import Data.Foldable (traverse_)
+import System.Process (spawnCommand, waitForProcess)
+import Data.Text.Conversions (convertText)
+import Paths_DMSS ( version )
+import Data.Version ( showVersion )
+
+desc :: Description
+desc = Description "Run tests for DMSS. To run all tests just run this script without any arguments."
+
+data UserInput = Test Bool (Maybe Text) | Version Bool deriving Show
+
+parser :: Parser UserInput
+parser = Test <$> subcommand "test" "Run tests" (switch "watch" 'w' "Watch for file changes.")
+              <*> optional (optText "pattern" 'p' "Run test with this pattern in test title")
+     <|> Version <$> subcommand "version" "Show version" (switch "something" 's' "Yeah")
+
+stackCommand :: Text
+stackCommand = "stack test --fast --haddock-deps"
+
+targets :: [Text]
+targets = [ ":DMSS-test"
+          , ":dev"
+          , ":dmss"
+          , ":dmssd"
+          ]
+
+main :: IO ExitCode
+main = do
+  userInput <- options desc parser
+  case userInput of
+    Test False Nothing -> runTests Nothing
+    Test True  Nothing -> watchTests Nothing
+    Test False str     -> runTests str
+    Test True  str     -> watchTests str
+    Version _          -> do
+      traverse_ echo (textToLine $ convertText $ showVersion version)
+      exit ExitSuccess
+  where
+    watchTests Nothing  = shell' (format (s%" --file-watch "%s) stackCommand targetsString)
+    watchTests (Just p) = shell' (format
+                                      (s%" --file-watch "%s%" "%s)
+                                      stackCommand targetsString (testArgs p))
+    runTests Nothing   = shell' (format (s%" "%s) stackCommand targetsString)
+    runTests (Just p)  = shell' (format (s%" "%s%" "%s) stackCommand targetsString (testArgs p))
+    targetsString = concat $ intersperse " " targets
+    testArgs = format ("--test-arguments '-p \""%s%"\"'")
+    -- Shell which allows for user input
+    -- https://github.com/Gabriel439/Haskell-Turtle-Library/issues/275
+    shell' :: Text -> IO ExitCode
+    shell' t = do
+      traverse_ echo $ textToLine t
+      spawnCommand (convertText t) >>= waitForProcess
